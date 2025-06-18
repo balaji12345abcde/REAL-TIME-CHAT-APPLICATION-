7c89501498c185c0d561f358841209dd449c3022
balaji12345abcde <balaji405791@gmail.com>
2025-06-18 16:32:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chat from './Chat';
+
+// Root component
+function App() {
+  return (
+    <div>
+      <h2>CODTECH Chat App</h2>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
